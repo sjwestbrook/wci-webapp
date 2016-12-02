7daab815ac8de79dc1c9523da69517cac96d7c81
Sarah Westbrook <sjwestbrook@users.noreply.github.com>
2016-12-02 11:00:56 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -8,14 +8,14 @@ routerApp.config(function($stateProvider, $urlRouterProvider) {
         
         // HOME STATES AND NESTED VIEWS ========================================
         .state('home', {
-            url: 'home',
-            templateUrl: 'partials/partial-home.html'
+            url: '/home',
+            templateUrl: 'partial-home.html'
         })
         
         // nested list with custom controller
         .state('home.list', {
-            url: 'list',
-            templateUrl: 'partials/partial-home-list.html',
+            url: '/list',
+            templateUrl: 'partial-home-list.html',
             controller: function($scope) {
                 $scope.dogs = ['Bernese', 'Husky', 'Goldendoodle'];
             }
@@ -23,18 +23,18 @@ routerApp.config(function($stateProvider, $urlRouterProvider) {
         
         // nested list with just some random string data
         .state('home.paragraph', {
-            url: 'paragraph',
+            url: '/paragraph',
             template: 'I could sure use a drink right now.'
         })
         
         // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
         .state('about', {
-            url: 'about',
+            url: '/about',
             views: {
                 '': { templateUrl: 'partial-about.html' },
                 'columnOne@about': { template: 'Look I am a column!' },
                 'columnTwo@about': { 
-                    templateUrl: 'partials/table-data.html',
+                    templateUrl: 'table-data.html',
                     controller: 'scotchController'
                 }
             }
