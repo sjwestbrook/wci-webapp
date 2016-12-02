d240c208e2c9ab4c41c30ba23482a1c66a4ce333
Sarah Westbrook <sjwestbrook@users.noreply.github.com>
2016-12-02 12:14:55 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -9,57 +9,81 @@ routerApp.config(function($stateProvider, $urlRouterProvider) {
         // HOME STATES AND NESTED VIEWS ========================================
         .state('home', {
             url: '/home',
-            templateUrl: './partials/partial-home.html'
+            templateUrl: './partials/home.html'
         })
         
+        .state('404', {
+            url: '/404',
+            templateUrl: './partials/404.html'
+        })
+    
+        .state('aca', {
+            url: '/aca',
+            templateUrl: './partials/aca.html'
+        })
+    
+        .state('board', {
+            url: '/board',
+            templateUrl: './partials/board.html'
+        })
+    
+       .state('home', {
+            url: '/home',
+            templateUrl: './partials/home.html'
+        })
+    
+    
+    
+    
+    
+    
         // nested list with custom controller
-        .state('home.list', {
-            url: '/list',
-            templateUrl: './partials/partial-home-list.html',
-            controller: function($scope) {
-                $scope.dogs = ['Bernese', 'Husky', 'Goldendoodle'];
-            }
-        })
+//         .state('home.list', {
+//             url: '/list',
+//             templateUrl: './partials/partial-home-list.html',
+//             controller: function($scope) {
+//                 $scope.dogs = ['Bernese', 'Husky', 'Goldendoodle'];
+//             }
+//         })
         
-        // nested list with just some random string data
-        .state('home.paragraph', {
-            url: '/paragraph',
-            template: 'I could sure use a drink right now.'
-        })
+//         // nested list with just some random string data
+//         .state('home.paragraph', {
+//             url: '/paragraph',
+//             template: 'I could sure use a drink right now.'
+//         })
         
         // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
         .state('about', {
             url: '/about',
             views: {
-                '': { templateUrl: './partials/partial-about.html' },
-                'columnOne@about': { template: 'Look I am a column!' },
-                'columnTwo@about': { 
-                    templateUrl: './partials/table-data.html',
-                    controller: 'scotchController'
-                }
-            }
-            
+                '': { templateUrl: './partials/about.html' },
+//                 'columnOne@about': { template: 'Look I am a column!' },
+//                 'columnTwo@about': { 
+//                     templateUrl: './partials/table-data.html',
+//                     controller: 'scotchController'
+//                 }
+            }            
         });
         
 });
 
-routerApp.controller('scotchController', function($scope) {
+// routerApp.controller('scotchController', function($scope) {
     
-    $scope.message = 'test';
+//     $scope.message = 'test';
    
-    $scope.scotches = [
-        {
-            name: 'Macallan 12',
-            price: 50
-        },
-        {
-            name: 'Chivas Regal Royal Salute',
-            price: 10000
-        },
-        {
-            name: 'Glenfiddich 1937',
-            price: 20000
-        }
-    ];
+//     $scope.scotches = [
+//         {
+//             name: 'Macallan 12',
+//             price: 50
+//         },
+//         {
+//             name: 'Chivas Regal Royal Salute',
+//             price: 10000
+//         },
+//         {
+//             name: 'Glenfiddich 1937',
+//             price: 20000
+//         }
+//     ];
     
-});
+// });
