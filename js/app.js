5688d6f75ce1564fcc513cfa10222f23623f9903
Sarah Westbrook <sjwestbrook@users.noreply.github.com>
2016-12-06 16:34:16 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -187,9 +187,21 @@ routerApp.config(function($stateProvider, $urlRouterProvider) {
             templateUrl: './partials/second-chances.html'
         })
     
+//        .state('staff', {
+//             url: '/staff',
+//             templateUrl: './partials/staff.html'
+//         })
+    
        .state('staff', {
             url: '/staff',
-            templateUrl: './partials/staff.html'
+            views: {
+                '': { templateUrl: './partials/staff.html' },
+//                 'columnOne@staff': { template: 'Look I am a column!' },
+//                 'columnTwo@staff': { 
+                    templateUrl: './partials/staff-list.html',
+                    controller: 'staffController'
+                }
+            }            
         })
     
         .state('success-stories', {
@@ -255,23 +267,43 @@ routerApp.config(function($stateProvider, $urlRouterProvider) {
     
 });
 
-// routerApp.controller('scotchController', function($scope) {
+routerApp.controller('staffController', function($scope) {
     
-//     $scope.message = 'test';
+    $scope.message = 'test';
    
-//     $scope.scotches = [
-//         {
-//             name: 'Macallan 12',
-//             price: 50
-//         },
-//         {
-//             name: 'Chivas Regal Royal Salute',
-//             price: 10000
-//         },
-//         {
-//             name: 'Glenfiddich 1937',
-//             price: 20000
-//         }
-//     ];
+    $scope.staff = [
+        {
+//             image: ,
+            name: 'Pamela Amundson',
+            title: 'Financial Manager - Fiscal',
+            location: 'La Crosse County',
+            phone: '608-789-5640',
+            email: 'amundsonp@workforceconnections.org'   
+        },
+        {
+//             image: ,
+            name: 'Carla Anderson',
+            title: 'Employment Resource Coordiator',
+            location: 'Monroe County',
+            phone: '608-374-7740',
+            email: 'andersonc@workforceconnections.org'
+        },
+        {
+//             image: ,
+            name: 'Kristin Bell',
+            title: 'Employment Coordinator - W2/FSET',
+            location: 'Jackson County',
+            phone: '715-284-7990',
+            email: 'bellk@workforceconnections.org'
+        },
+        {
+//             image: ,
+            name: 'Autumn Bergerson',
+            title: 'Employment Coordinator - W2/FSET',
+            location: 'Trempealeau, Buffalo, Pepin Counties',
+            phone: '608-790-2380',
+            email: 'bergersona@workforceconnections.org'
+        }
+    ];
     
-// });
+});
