5c37ff284848f34e63bbd9f06023fe5c00f2cbb5
Sarah Westbrook <sjwestbrook@users.noreply.github.com>
2016-12-07 12:23:09 -0700

    Update boardController.js

--- js/boardController.js
+++ js/boardController.js
@@ -1,1 +1,64 @@
 
+routerApp.controller('staffController', function($scope) {
+    
+    $scope.message = 'test';
+   
+    $scope.staff = [
+
+        {
+            name: 'Ann Boland',
+            role: 'Chair',
+            company: 'Ingersoll Rand',
+            title: 'Director, Service Delivery and Relationship Management'
+        },
+        {
+            name: 'David Ring',
+            role: 'Vice-Chair',
+            company: 'KwikTrip Inc.',
+            title: 'Community Relations Coordinator'
+        },
+        {
+            name: 'Kevin Zagrodnick',
+            role: 'Secretary/Treasurer',
+            company: 'River City Bank',
+            title: 'Assistant Vice President'
+        },
+        {
+            name: 'Kurt Bear',
+            role: 'Director',
+            company: 'MidCity Steel',
+            title: 'Owner/CEO'
+        },
+        {
+            name: '',
+            role: 'Director',
+            company: '',
+            title: ''
+        },
+        {
+            name: '',
+            role: 'Director',
+            company: '',
+            title: ''
+        },
+        {
+            name: '',
+            role: 'Director',
+            company: '',
+            title: ''
+        },
+        {
+            name: '',
+            role: 'Director',
+            company: '',
+            title: ''
+        },
+        {
+            name: '',
+            role: 'Director',
+            company: '',
+            title: ''
+        }
+    ];
+    
+});
