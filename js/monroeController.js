b181a647c40d86e8ea5f17b25ebb63a12f703c90
Sarah Westbrook <sjwestbrook@users.noreply.github.com>
2016-12-16 12:18:32 -0700

    Create monroeController.js

---
+++ js/monroeController.js
@@ -0,0 +1,19 @@
+routerApp.controller('monroeController', function($scope) {
+    
+    $scope.message = 'test';
+   
+    $scope.monroe = [
+        {
+            name: 'Melissa Hansen',
+            title: 'Employment Coordinator - W2/FSET',
+            phone: '608-433-2783',
+            email: 'hansenm@workforceconnections.org'
+        },
+        
+        {
+            name: 'Sadie Schultz',
+            title: 'Employment785-7740-2769',
+            email: 'schultzs@workforceconnections.org'
+        }
+    ];    
+});
