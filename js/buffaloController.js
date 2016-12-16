f3e43c9484f92fc1440fea6530a1d12bc625ec4f
Sarah Westbrook <sjwestbrook@users.noreply.github.com>
2016-12-16 10:48:42 -0700

    Update buffaloController.js

--- js/buffaloController.js
+++ js/buffaloController.js
@@ -1,1 +1,22 @@
-
+routerApp.controller('buffaloController', function($scope) {
+    
+    $scope.message = 'test';
+   
+    $scope.buffaloStaff = [
+        {
+//             image: ,
+            name: 'Autumn Bergerson',
+            title: 'Employment Coordinator - W2/FSET',
+            phone: '608-790-2380',
+            email: 'bergersona@workforceconnections.org'
+        },
+        
+        {
+//             image: ,
+            name: 'Pam Taylor',
+            title: 'Employment Coordinator - WIOA',
+            phone: '608-386-1629',
+            email: 'taylorp@workforceconnections.org'
+        }
+    ];    
+});
