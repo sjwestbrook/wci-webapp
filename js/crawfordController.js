f9c754d878355c54c993abde8faeadb1350ff8f7
Sarah Westbrook <sjwestbrook@users.noreply.github.com>
2016-12-16 11:53:27 -0700

    Update crawfordController.js

--- js/crawfordController.js
+++ js/crawfordController.js
@@ -4,17 +4,17 @@ routerApp.controller('crawfordController', function($scope) {
    
     $scope.crawford = [
         {
-            name: 'Autumn Bergerson',
-            title: 'Employment Coordinator - W2/FSET',
-            phone: '608-790-2380',
-            email: 'bergersona@workforceconnections.org'
+            name: 'Tyler Diktanas',
+            title: 'Employment Coordinator - WIOA, W2/FSET',
+            phone: '608-433-2775',
+            email: 'diktanast@workforceconnections.org'
         },
         
         {
-            name: 'Pam Taylor',
-            title: 'Employment Coordinator - WIOA',
-            phone: '608-386-1629',
-            email: 'taylorp@workforceconnections.org'
+            name: 'Frankie Mezera',
+            title: 'Employment Coordinator - W2/FSET',
+            phone: '608-433-2769',
+            email: 'mezeraf@workforceconnections.org'
         }
     ];    
 });
