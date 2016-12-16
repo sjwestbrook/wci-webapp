74f6a78e9dfabe31c9278864383f7a97f609706b
Sarah Westbrook <sjwestbrook@users.noreply.github.com>
2016-12-16 12:21:51 -0700

    Create pepinController.js

---
+++ js/pepinController.js
@@ -0,0 +1,14 @@
+routerApp.controller('pepinController', function($scope) {
+    
+    $scope.message = 'test';
+   
+    $scope.pepin = [
+        {
+            name: 'Autumn Bergerson',
+            title: 'Employment Coordinator - W2/FSET',
+            phone: '608-790-2380',
+            email: 'bergersona@workforceconnections.org'
+        }
+        
+    ];    
+});
