a05c7981cc69fd289471ff4d33500b87a669c431
Sarah Westbrook <sjwestbrook@users.noreply.github.com>
2016-12-16 11:48:36 -0700

    Create crawfordController.js

---
+++ js/crawfordController.js
@@ -0,0 +1,20 @@
+routerApp.controller('crawfordController', function($scope) {
+    
+    $scope.message = 'test';
+   
+    $scope.crawford = [
+        {
+            name: 'Autumn Bergerson',
+            title: 'Employment Coordinator - W2/FSET',
+            phone: '608-790-2380',
+            email: 'bergersona@workforceconnections.org'
+        },
+        
+        {
+            name: 'Pam Taylor',
+            title: 'Employment Coordinator - WIOA',
+            phone: '608-386-1629',
+            email: 'taylorp@workforceconnections.org'
+        }
+    ];    
+});
